d913280f1d4e4ebe746407cd5d462ed467ae3917
charlie.duhor@gmail.com <charlie.duhor@gmail.com@ec6b322e-8093-32e6-095c-f9950440269a>
2011-09-05 13:04:12 +0000

    Initial import

---
+++ trunk/DTSShape.cpp
@@ -0,0 +1,369 @@
+/*
+ * dts2fbx
+ * Copyright (c) 2011 Charlie Duhor. All rights reserved.
+ *
+ * @DTS2FBX_LICENSE_HEADER_START@
+ * 
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * 
+ * @DTS2FBX_LICENSE_HEADER_START@
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <vector>
+#include <sys/stat.h>
+
+#include "DTSTypes.h"
+#include "DTSBase.h"
+#include "DTSShape.h"
+
+DTSShape::DTSShape(FILE* file) : DTSBase(file)
+{
+    Read(numNodes);
+    Read(numObjects);
+    Read(numDecals);
+    Read(numSubshapes);
+    Read(numIFLmaterials);
+    
+    if (dtsVersion < 22)
+    {
+        Read(numNodeRotations);
+        numNodeRotations      -= numNodes;
+        numNodeTranslations    = numNodeRotations;
+        numNodeScalesUniform   = 0;
+        numNodeScalesAligned   = 0;
+        numNodeScalesArbitrary = 0;      
+        numGroundFrames        = 0;
+    }
+    else
+    {
+        Read(numNodeRotations) ;
+        Read(numNodeTranslations);
+        Read(numNodeScalesUniform);
+        Read(numNodeScalesAligned);
+        Read(numNodeScalesArbitrary);
+        
+        if (dtsVersion > 23)
+        {
+            Read(numGroundFrames);
+        }
+    }
+
+    Read(numObjectStates);
+    Read(numDecalStates);
+    Read(numTriggers);
+    Read(numDetailLevels);
+    Read(numMeshes);
+    
+    if (dtsVersion < 23)
+        Read(numSkins);
+    else
+        numSkins = 0;
+    
+    Read(numNames);
+    
+    int smallestSizeInt;
+    Read(smallestSizeInt);
+    Read(smallestDetailLevel);
+    smallestSize = (float)smallestSizeInt ;
+    
+    ReadCheck(0);
+    
+    // Bounds
+    Read(radius);
+    Read(tubeRadius);
+    Read(center);
+    Read(bounds);
+    
+    ReadCheck(1);
+    
+    // Nodes
+    
+    nodes.resize(numNodes);
+    Read(nodes);
+    ReadCheck(2);
+    
+    // Objects 
+    
+    objects.resize(numObjects);
+    Read(objects);
+    ReadCheck(3);
+    
+    // Decals
+    
+    decals.resize(numDecals);
+    Read(decals);
+    ReadCheck(4);
+    
+    // IFL Materials
+    
+    IFLmaterials.resize(numIFLmaterials);
+    Read(IFLmaterials);
+    ReadCheck(5);
+    
+    // Subshapes
+    
+    subshapes.resize(numSubshapes) ;
+    std::vector<DTSSubshape>::iterator shape ;
+    for (shape = subshapes.begin() ; shape != subshapes.end() ; shape++)
+        Read(shape->firstNode);
+    for (shape = subshapes.begin() ; shape != subshapes.end() ; shape++)
+        Read(shape->firstObject);
+    for (shape = subshapes.begin() ; shape != subshapes.end() ; shape++)
+        Read(shape->firstDecal);
+    ReadCheck(6) ;
+    for (shape = subshapes.begin() ; shape != subshapes.end() ; shape++)
+        Read(shape->numNodes);
+    for (shape = subshapes.begin() ; shape != subshapes.end() ; shape++)
+        Read(shape->numObjects);
+    for (shape = subshapes.begin() ; shape != subshapes.end() ; shape++)
+        Read(shape->numDecals);
+    ReadCheck(7) ;
+    
+    // MeshIndexList (obsolete data)
+    
+    if (dtsVersion < 16)
+    {
+        int size, temp ;
+        Read(size);
+        while (size--) Read(temp);
+    }
+    
+    // Default translations and rotations
+    
+    nodeDefRotations   .resize(numNodes) ;
+    nodeDefTranslations.resize(numNodes) ;
+    
+    for (size_t n = 0 ; n < nodes.size() ; n++)
+    {
+        Read(nodeDefRotations[n]);
+        Read(nodeDefTranslations[n]);
+    }
+    
+    // Animation translations and rotations
+    
+    nodeTranslations.resize(numNodeTranslations);
+    nodeRotations   .resize(numNodeRotations);
+    Read(nodeTranslations);
+    Read(nodeRotations);
+    ReadCheck(8);
+    
+    // Default scales
+    
+    nodeScalesUniform  .resize(numNodeScalesUniform);
+    nodeScalesAligned  .resize(numNodeScalesAligned);
+    nodeScalesArbitrary.resize(numNodeScalesArbitrary);
+    
+    if (dtsVersion > 21)
+    {
+        Read(nodeScalesUniform);
+        Read(nodeScalesAligned);
+        Read(nodeScalesArbitrary);
+        Read(nodeScaleRotsArbitrary);
+        ReadCheck(9) ;
+    }
+    
+    // Ground transformations
+   
+    groundTranslations.resize(numGroundFrames);
+    groundRotations   .resize(numGroundFrames);
+    
+    if (dtsVersion > 23)
+    {
+        Read(groundTranslations);
+        Read(groundRotations);
+        ReadCheck(10);
+    }
+    
+    // Object states
+    
+    objectStates.resize(numObjectStates) ;
+    Read(objectStates);
+    ReadCheck(11);
+    
+    // Decal states
+    
+    decalStates.resize(numDecalStates);
+    Read(decalStates);
+    ReadCheck(12);
+    
+    // Triggers
+    
+    triggers.resize(numTriggers);
+    Read(triggers);
+    ReadCheck(13);
+    
+    // Detail Levels
+    
+    detailLevels.resize(numDetailLevels);
+    Read(detailLevels);
+    ReadCheck(14);
+    
+    // Meshes
+    
+    meshes.resize(numMeshes);
+    Read(meshes);
+    ReadCheck();
+
+    // Names
+    
+    names.resize(numNames);
+    Read(names);
+    ReadCheck();
+
+	int numSequences = ReadRawTyped<int>(file);
+
+	sequences.resize(numSequences);
+
+	for (size_t seq = 0 ; seq < sequences.size() ; seq++)
+	{
+		DTSSequence& p = sequences[seq];
+
+		p.nameIndex        = ReadRawTyped<int>(file);
+		p.flags            = ReadRawTyped<int>(file);
+		p.numKeyFrames     = ReadRawTyped<int>(file);
+		p.duration         = ReadRawTyped<float>(file);
+		p.priority         = ReadRawTyped<int>(file);
+		p.firstGroundFrame = ReadRawTyped<int>(file);
+		p.numGroundFrames  = ReadRawTyped<int>(file);
+		p.baseRotation     = ReadRawTyped<int>(file);
+		p.baseTranslation  = ReadRawTyped<int>(file);
+		p.baseScale        = ReadRawTyped<int>(file);
+		p.baseObjectState  = ReadRawTyped<int>(file);
+		p.baseDecalState   = ReadRawTyped<int>(file);
+		p.firstTrigger     = ReadRawTyped<int>(file);
+		p.numTriggers      = ReadRawTyped<int>(file);
+		p.toolBegin        = ReadRawTyped<float>(file);
+
+		ReadRawTyped(file, p.matters.rotation);
+		ReadRawTyped(file, p.matters.translation);
+		ReadRawTyped(file, p.matters.scale);
+		ReadRawTyped(file, p.matters.decal);
+		ReadRawTyped(file, p.matters.ifl);
+		ReadRawTyped(file, p.matters.vis);
+		ReadRawTyped(file, p.matters.frame);
+		ReadRawTyped(file, p.matters.matframe);
+	}
+
+	// Materials
+
+	/*char materialListVersion =*/ ReadRawTyped<char>(file);
+	int  materialCount       = ReadRawTyped<int> (file);
+
+	materials.resize(materialCount);
+
+	std::vector<DTSMaterial>::iterator mat;
+	for (mat = materials.begin(); mat != materials.end() ; mat++)
+	{
+		DTSMaterial&  material = *mat;
+		unsigned char length   = ReadRawTyped<unsigned char>(file);
+
+		material.name.resize(length);
+		fread((char*)material.name.data(), length, 1, file);
+	}
+
+	for (mat = materials.begin() ; mat != materials.end() ; mat++)
+		(*mat).flags = ReadRawTyped<int>(file);
+	for (mat = materials.begin() ; mat != materials.end() ; mat++)
+		(*mat).reflectance = ReadRawTyped<int>(file);
+	for (mat = materials.begin() ; mat != materials.end() ; mat++)
+		(*mat).bump = ReadRawTyped<int>(file);
+	for (mat = materials.begin() ; mat != materials.end() ; mat++)
+		(*mat).detail = ReadRawTyped<int>(file);
+	for (mat = materials.begin() ; mat != materials.end() ; mat++)
+		(*mat).detailScale = ReadRawTyped<int>(file);
+	for (mat = materials.begin() ; mat != materials.end() ; mat++)
+		(*mat).reflection = ReadRawTyped<int>(file);
+}
+
+std::string DTSShape::nodeNameAtIndex(int index) const
+{
+    if (index < 0)
+    {
+        return "(null)";
+    }
+    
+    return names[nodes[index].name];
+}
+
+std::string DTSShape::objectNameAtIndex(int index) const
+{
+    if (index < 0)
+    {
+        return "(null)";
+    }
+    
+    return names[objects[index].name];
+}
+
+std::string DTSShape::decalNameAtIndex(int index) const
+{
+    if (index < 0)
+    {
+        return "(null)";
+    }
+    
+    if (index >= (int)decals.size())
+    {
+        return "(invalid)";
+    }
+    
+    return names[decals[index].name];
+}
+
+#ifdef WIN32
+#define PATHSEP "\\"
+#else
+#define PATHSEP "/"
+#endif
+
+void DTSResolver::addPathContaining(const std::string& n)
+{
+    size_t lastEntry = n.rfind(PATHSEP);
+    
+    if (lastEntry == std::string::npos)
+    {
+        paths.push_back(n);
+    }
+    else
+    {
+        paths.push_back(n.substr(0, lastEntry));
+    }
+}
+
+std::string DTSResolver::resolve(const std::string& n) const
+{
+    struct stat s;
+    
+    std::vector<std::string>::const_iterator it, end(paths.end());
+    
+    for (it = paths.begin(); it != end; ++it)
+    {
+        std::string e(*it);
+        
+        e += PATHSEP;
+        e += n;
+        
+        if (stat(e.c_str(), &s) == 0)
+        {
+            return e;
+        }
+    }
+    
+    return n;
+}
---
+++ trunk/DTSShape.h
@@ -0,0 +1,303 @@
+/*
+ * dts2fbx
+ * Copyright (c) 2011 Charlie Duhor. All rights reserved.
+ *
+ * @DTS2FBX_LICENSE_HEADER_START@
+ * 
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * 
+ * @DTS2FBX_LICENSE_HEADER_START@
+ */
+
+#ifndef DTSConverter_DTSShape_h
+#define DTSConverter_DTSShape_h
+
+#include "DTSBase.h"
+
+#include <string>
+
+class DTSNode
+{
+public:
+    int name;
+    int parent;
+    int firstObject;
+    int child;
+    int sibling;
+};
+
+class DTSObject
+{
+public:
+    int name;
+    int numMeshes;
+    int firstMesh;
+    int node;
+    int sibling;
+    int firstDecal;
+};
+
+class DTSDecal
+{
+public:
+    int name;
+    int numMeshes;
+    int firstMesh;
+    int object;
+    int sibling;
+};
+
+class DTSIFLMaterial
+{
+public:
+    int name;
+    int slot;
+    int firstFrame;
+    int time;
+    int numFrames;
+};
+
+class DTSSubshape
+{
+public:
+    int firstNode;
+    int firstObject;
+    int firstDecal;
+    int numNodes;
+    int numObjects;
+    int numDecals;
+    int firstTranslucent;
+};
+
+class DTSObjectState
+{
+public:
+    float vis;
+    int   frame;
+    int   matFrame;
+};
+
+class DTSDetailLevel
+{
+public:
+    int   name;
+    int   subshape;
+    int   objectDetail;
+    float size;
+    float avgError;
+    float maxError;
+    int   polyCount;
+};
+
+class DTSTrigger
+{
+public:
+    int   state;
+    float pos;
+};
+
+class DTSDecalState
+{
+public:
+    int frame;
+};
+
+class DTSPrimitive
+{
+public:
+    short firstElement;
+    short numElements;
+    int   type;
+};
+
+class DTSCluster
+{
+public:
+	int     startPrimitive;
+	int     endPrimitive;
+	Point   normal;
+	float   k;
+	int     frontCluster;
+	int     backCluster;
+};
+
+class DTSMesh
+{
+public:
+    enum
+    {
+        T_Standard = 0,
+        T_Skin     = 1,
+        T_Decal    = 2,
+        T_Sorted   = 3,
+        T_Null     = 4
+    };
+    
+public:
+    int   type;
+    int   numFrames;
+    int   matFrames;
+    int   parent;
+    Box   bounds;
+    Point center;
+    float radius;
+    
+    std::vector<Point>         verts;
+    std::vector<Point2D>       tverts;
+    std::vector<Point>         normals;
+    std::vector<unsigned char> enormals;
+    
+    std::vector<DTSPrimitive>   primitives;
+    std::vector<unsigned short> indices;
+    std::vector<unsigned short> mindices;
+    
+    int vertsPerFrame;
+    int flags;
+
+	// Skin data
+	std::vector<int>          vindex;
+	std::vector<int>          vbone;
+	std::vector<float>        vweight;
+	std::vector<int>          nodeIndex;
+	std::vector<Matrix<4,4> > nodeTransform;
+
+	// Decal data
+	std::vector<DTSCluster> clusters;
+	std::vector<int>        startCluster;
+	std::vector<int>        firstVerts;
+	std::vector<int>        numVerts;
+	std::vector<int>        firstTVerts;
+};
+
+class DTSSequence
+{
+public:
+	int   nameIndex;
+	int   flags;
+	int   numKeyFrames;
+	float duration;
+	int   priority;
+	int   firstGroundFrame;
+	int   numGroundFrames;
+	int   baseRotation;
+	int   baseTranslation;
+	int   baseScale;
+	int   baseObjectState;
+	int   baseDecalState;
+	int   firstTrigger;
+	int   numTriggers;
+	float toolBegin;
+
+    struct matters_array {
+        std::vector<bool> rotation;
+        std::vector<bool> translation;
+        std::vector<bool> scale;
+        std::vector<bool> decal;
+        std::vector<bool> ifl;
+        std::vector<bool> vis;
+        std::vector<bool> frame;
+        std::vector<bool> matframe;
+    } matters;
+};
+
+class DTSMaterial
+{
+public:
+	std::string name;
+	int         flags;
+	int         reflectance;
+	int         bump;
+	int         detail;
+	int         detailScale;
+	int         reflection;
+};
+
+class DTSResolver
+{
+public:
+    std::vector<std::string> paths;
+    
+public:
+    void addPathContaining(const std::string&);
+    
+    std::string resolve(const std::string&) const;
+};
+
+class DTSShape : public DTSBase
+{
+public:
+    int numNodes;
+    int numObjects;
+    int numDecals;
+    int numSubshapes;
+    int numIFLmaterials;
+    int numNodeRotations;
+    int numNodeTranslations;
+    int numNodeScalesUniform;
+    int numNodeScalesAligned;
+    int numNodeScalesArbitrary;
+    int numGroundFrames;
+    int numObjectStates;
+    int numDecalStates;
+    int numTriggers;
+    int numDetailLevels;
+    int numMeshes;
+    int numSkins;
+    int numNames;
+    
+    float smallestSize;
+    int   smallestDetailLevel;
+    
+    float radius;
+    float tubeRadius;
+    Point center;
+    Box   bounds;
+    
+    std::vector<DTSNode>        nodes;
+    std::vector<DTSObject>      objects;
+    std::vector<DTSDecal>       decals;
+    std::vector<DTSIFLMaterial> IFLmaterials;
+    std::vector<DTSSubshape>    subshapes;
+
+    std::vector<Quaternion>   nodeDefRotations;
+    std::vector<Point>        nodeDefTranslations;
+    std::vector<Quaternion>   nodeRotations;
+    std::vector<Point>        nodeTranslations;
+    std::vector<float>        nodeScalesUniform;
+    std::vector<Point>        nodeScalesAligned;
+    std::vector<Point>        nodeScalesArbitrary;
+    std::vector<Quaternion>   nodeScaleRotsArbitrary;
+    std::vector<Quaternion>   groundRotations;
+    std::vector<Point>        groundTranslations;
+    
+    std::vector<DTSObjectState> objectStates;
+    std::vector<DTSDecalState>  decalStates;
+    std::vector<DTSDetailLevel> detailLevels;
+    std::vector<DTSTrigger>     triggers;
+    std::vector<DTSMesh>        meshes;
+    std::vector<DTSSequence>    sequences;
+    std::vector<std::string>    names;
+	std::vector<DTSMaterial>    materials;
+    
+public:
+    DTSShape(FILE*);
+    
+    std::string nodeNameAtIndex(int) const;
+    std::string objectNameAtIndex(int) const;
+    std::string decalNameAtIndex(int) const;
+};
+
+#endif
